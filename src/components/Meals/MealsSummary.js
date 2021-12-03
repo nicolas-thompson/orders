219589925fc2efa4375cbadd6d64afbf4fbb207d
Nicolas Thompson <mail@nicolasthompson.com>
2021-12-03 21:11:07 +0000

    Adds Meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+  <section className={classes.summary}>
+    <h2>Meals Summary</h2>
+    <p> This is the meals summary </p>
+  </section>
+  );
+};
+
+export default MealsSummary;
