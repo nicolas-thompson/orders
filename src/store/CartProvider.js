791a124f1bf8e4edf4885dfc1a68382b8582a132
Nicolas Thompson <mail@nicolasthompson.com>
2022-02-05 19:05:49 +0000

    Remove an from the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -35,17 +35,43 @@ const cartReducer = (state, action) => {
     }
   }
 
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+
+
   return defaultCartState;
 }
 
 const CartProvider = props => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-  const addItemToCartHandler = item => {
+  const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: 'ADD', item: item });
   }
 
-  const removeItemFromCartHandler = id => {
+  const removeItemFromCartHandler = (id) => {
     dispatchCartAction({ type: 'REMOVE', id: id })
   }
 
